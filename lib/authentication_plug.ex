e9de561747013a51c5941200e40ad79214ddf8b0
Taylor Howell <66977633+EarliestFall988@users.noreply.github.com>
2025-04-26 11:50:55 -0500

    removing the debug console output in the auth plug

--- lib/authentication_plug.ex
+++ lib/authentication_plug.ex
@@ -15,7 +15,7 @@ defmodule Clerk.AuthenticationPlug do
 
     with {:ok, session} <- Map.fetch(conn.req_cookies, session_key),
          {:ok, %{"sub" => user_id} = session} <- Clerk.Session.verify_and_validate(session),
-         {:ok, user} <- get_user(user_id) do
+         {:ok, user} <- Clerk.User.get(user_id) do
       conn |> Plug.Conn.assign(:clerk_session, session) |> Plug.Conn.assign(:current_user, user)
     else
       _ ->
@@ -24,19 +24,4 @@ defmodule Clerk.AuthenticationPlug do
         |> Plug.Conn.halt()
     end
   end
-
-
-  # private function to get the user from Clerk. If the user is not found, it will return an error.
-  # Also includes a message to the console if the user is not found. Hopefully it will guide the user
-  # if they have not setup the domain and secret key properly.
-  # 👇
-  defp get_user(user_id) do
-    case Clerk.User.get(user_id) do
-      {:ok, user} -> {:ok, user}
-      {:error, _} ->
-        IO.puts("Error fetching the user from Clerk (have you set the domain, and secret key?)")
-        {:error, :user_not_found}
-    end
-  end
-
 end
